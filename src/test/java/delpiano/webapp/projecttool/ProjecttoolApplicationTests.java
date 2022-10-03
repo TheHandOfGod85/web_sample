4ffb3ccc9590ae1311b71619522d5692141dcf0a
Daniele Del Piano <daniele.delpiano30@gmail.com>
2022-10-03 18:11:25 +0100

    V1 plain project

---
+++ src/test/java/delpiano/webapp/projecttool/ProjecttoolApplicationTests.java
@@ -0,0 +1,13 @@
+package delpiano.webapp.projecttool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecttoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
